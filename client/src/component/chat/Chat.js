bc3d45390b798fc8209562e8d1f1a22bd91f6253
himanshu1004200 <chaharhimanshu03@gmail.com>
2024-01-04 15:54:28 +0530

    basic messages when enters the room

--- client/src/component/chat/Chat.js
+++ client/src/component/chat/Chat.js
@@ -2,21 +2,37 @@ import React, { useEffect } from 'react'
 import {user} from '../Join/Join'
 import socketIo from 'socket.io-client'
 import './chat.css'
+import sendLogo from '../../images/send.png'
 
 const ENDPOINT = 'http://localhost:4500/';
 
 
 function Chat() {
 
-    const socket = socketIo(ENDPOINT,{transports:['websocket']});
     useEffect(()=>{
+        const socket = socketIo(ENDPOINT,{transports:['websocket']});
+
         socket.on('connect',()=>{
             alert('connected');
         })
+        socket.emit('joined',{user});
+        socket.on('welcome',(data)=>{
+            console.log(data.user,data.message);
+        })
+        socket.on('userJoined',(data)=>{
+            console.log(data.user,data.message);
+        })
+        socket.on('leave',(data)=>{
+            console.log(data.user,data.message)
+        })
         return()=>{
+            socket.emit('disconnect');
+
+            socket.off();
+
 
         }
-    },[socket]);
+    },[]);
     
 
   return (
@@ -24,7 +40,13 @@ function Chat() {
         <div className="chatContainer">
             <div className="header"></div>
             <div className="chatBox"></div>
-            <div className="inputBox"></div>  
+            <div className="inputBox">
+                <input type="text" id='chatInput' />
+                <button className='sendBtn' ><img src={sendLogo} alt="" /></button>
+
+
+            </div>
+
         </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const socketIo = require('socket.io')
 const port = 4500 || process.env.port; 
 
 const cors = require('cors');
@@ -9,13 +10,27 @@ app.get('/',(req,res)=>{
     res.send('Running !!!')
 })
 
-const socketIo = require('socket.io')
+const users = [{}];
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // whenever connection established between frontend and backend -->> then it will be showing connection
-io.on("connection",()=>{
+io.on("connection",(socket)=>{
     console.log('New Connection');
+    socket.on('joined',({user})=>{
+        console.log(`${user} has joined.`)
+        users[socket.id] = user;
+        socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined the chat!`})
+        socket.emit('welcome',{user:"Admin",message:`Welcome to the chat! ${users[socket.id]}`});
+    })
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('leave',{user:'Admit',message:`${users[socket.id]} has left the chat!!`})
+        console.log('User has left the chat !!')
+    })
+    
+    // broadcast means jisne join kiya usey chodke baaki sbpe msg phuchega...
+   
 })
 
 server.listen(port,()=>{
